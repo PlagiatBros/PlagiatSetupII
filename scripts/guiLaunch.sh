1a063539cffa28cc75888d3fac8d4e5a8bc5ceca
Plagiat Gang Of Bros <plagiat@LaMachineACashDesBros.plagiat.org>
2022-03-07 18:04:51 +0100

    Some nu scripts

---
+++ scripts/guiLaunch.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "launching GUI"
+raysession -r /home/plagiat/PlagiatSetup/RaySessions -p 2000 > /home/plagiat/PlagiatSetup/log/raysession.log 2>&1 &
+
+echo "jack pretty naming"
+bash /home/plagiat/PlagiatSetup/scripts/jackPrettyNames.sh
