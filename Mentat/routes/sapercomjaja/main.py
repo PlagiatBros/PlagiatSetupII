804de92372004719c5b96cad70c83ceb870a1014
Plagiat Gang Of Bros <plagiat@LaMachineACashDesBros.plagiat.org>
2022-04-27 22:23:12 +0200

    Saper Comme Jaja Routes Finished

--- Mentat/routes/sapercomjaja/main.py
+++ Mentat/routes/sapercomjaja/main.py
@@ -116,6 +116,15 @@ class AgneauGastrik(Video, Light, RouteBase):
         vocalsNano.set('gars_exclu', 'on')
         vocalsKesch.set('normo_exclu', 'on')
 
+    @mk2_button(2) # à l'origine bouton 6
+    def zynette_dre(self):
+        """
+        Zynette Dre
+        """
+
+        # Sequences
+        seq192.select('on', 'zynette_*')
+
 
     @pedalboard_button(3) # bouton 6 à l'origine
     def mandelaaa2(self):
@@ -136,9 +145,58 @@ class AgneauGastrik(Video, Light, RouteBase):
         vocalsNano.set('gars_exclu', 'on')
         vocalsKesch.set('meuf_exclu', 'on')
 
+    @mk2_button(2) # à l'origine bouton 6
+    def zynette_dre2(self):
+        """
+        Zynette Dre (cf. zynette_dre)
+        """
+        # méthode vide juste pour que le déroulé du morceau appairaisse de façon linéaire
+        pass
+
     @pedalboard_button(6) # bouton 9 à l'origine
     def eggz_trapcut(self):
         """
         Mamaz Bakingz Eggz
         """
         #### TODO scène trapcut
+
+    @pedalboard_button(7) # bouton 10 à l'origine
+    def rimdogged(self):
+        """
+        RimDoooooooooged
+        """
+
+        # Transport
+        transport.stop()
+
+        # Vocals
+        vocalsNano.set('normo_exclu', 'on')
+        vocalsNanoFX4Disint.set('NanoNormo', 'Gain', 'Mute', 0.0)
+
+        # Keyboards
+        jmjKeyboard.set_sound('CDubstepHorn')
+
+    @pedalboard_button(8) # bouton 11 à l'origine
+    def mandelaaa_final(self):
+        """
+        Mandela A A A A Final
+        """
+
+        # Sequences
+        seq192.select('solo', 'mandelaaa_final_*')
+
+        # Transport
+        transport.set_cycle('4/4', pattern="Xxxx")
+        transport.start()
+
+        # Samples
+        samples.set('Samples4', 'Gain', 'Mute', 0.0)
+
+        # Vocals
+        vocalsNano.set('normo_exclu', 'on')
+        vocalsKesch.set('meuf_exclu', 'on')
+
+        # Keyboards
+        jmjKeyboard.set_sound('CDubstepHorn')
+
+    #### TODO Méthodes boutons 7 & 8 Jeannot Cock your butter gun?
