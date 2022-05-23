7b333b42e9a5be499c863608231729b1c3aba959
Plagiat Gang Of Bros <plagiat@LaMachineACashDesBros.plagiat.org>
2022-05-23 18:17:33 +0200

    scripts: start OSC with --force-gpu

--- scripts/startOSC.sh
+++ scripts/startOSC.sh
@@ -1,2 +1,2 @@
 cd $PLAGIAT_SETUP_DIR/OpenStageControl
-open-stage-control -p 3000 -l main.json -c custom-module.js -t styles.css -s 127.0.0.1:2001 --disable-vsync
+taskset -a 0x01 open-stage-control -p 3000 -l main.json -c custom-module.js -t styles.css -s 127.0.0.1:2001 --force-gpu
