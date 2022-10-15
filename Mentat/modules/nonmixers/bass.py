cf51057a64717d49233122963c68b68d734fedbe
Plagiat Gang Of Bros <plagiat@LaMachineACashDesBros.plagiat.org>
2022-10-15 09:53:06 +0200

    Add Missing file

---
+++ Mentat/modules/nonmixers/bass.py
@@ -0,0 +1,27 @@
+from ..nonmixer import NonMixer
+
+class Bass(NonMixer):
+
+    def create_meta_parameters(self):
+
+        def getter(leduc_mute, fbass_mute):
+            if leduc_mute == 0.0 and fbass_mute == 1.0:
+                return 'leduc'
+            if leduc_mute == 1.0 and fbass_mute == 0.0:
+                return 'fbass'
+            else:
+                return '?'
+
+        def setter(source):
+            if source == 'leduc':
+                self.set('BassDry', 'Mute', 0.0)
+                self.set('FBassDry', 'Mute', 1.0)
+            elif source == 'fbass':
+                self.set('BassDry', 'Mute', 1.0)
+                self.set('FBassDry', 'Mute', 0.0)
+
+        self.add_meta_parameter(
+            'source',
+            [['BassDry' 'Mute'], ['FBassDry', 'Mute']],                                                   # params
+            getter, setter
+        )
