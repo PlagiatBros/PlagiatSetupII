c6cf2ea55f086116b77e97d786955272f7b9499a
bordun <jean-emmanuel.doucet@groolot.net>
2022-11-15 17:29:23 +0100

    script: osc start fix

--- scripts/startOSC.sh
+++ scripts/startOSC.sh
@@ -1,2 +1,2 @@
 cd $PLAGIAT_SETUP_DIR/OpenStageControl
-taskset -a 0x01 open-stage-control -p 3000 -l hub.json -c custom-module.js -t styles.css -s 127.0.0.1:2001 --force-gpu --disable-vsync --client-options id=main --fullscreen
+taskset -a 0x01 open-stage-control -p 3000 -l hub.json -c custom-module.js -t styles.css -s 127.0.0.1:2001 --force-gpu --disable-vsync --fullscreen
