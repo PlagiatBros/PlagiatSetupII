538c1609badce3b3c65e6e44becfc8f90b1bc775
Plagiat Gang Of Bros <plagiat@LaMachineACashDesBros.plagiat.org>
2023-04-04 08:09:31 +0200

    Cleaning directories

--- Mentat/routes/dirtydirtydirty/light.py
+++
@@ -1,8 +0,0 @@
-from ..base import mk2_button, pedalboard_button
-from modules import *
-
-class Light():
-
-    def part(self, *args, **kwargs):
-
-        pass
--- Mentat/routes/dirtydirtydirty/video.py
+++
@@ -1,8 +0,0 @@
-from ..base import mk2_button, pedalboard_button
-from modules import *
-
-class Video():
-
-    def part(self, *args, **kwargs):
-
-        pass
