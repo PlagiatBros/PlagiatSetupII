37d1e6bf7615edabc67c972ef3172c413671389c
Plagiat Gang Of Bros <plagiat@LaMachineACashDesBros.plagiat.org>
2023-04-04 08:06:35 +0200

    Fix file interversion

---
+++ Mentat/routes/ddd/main.py
@@ -0,0 +1,148 @@
+from ..base import *
+from .video import Video
+from .light import Light
+
+from modules import *
+
+class DDD(Video, Light, RouteBase):
+    """
+    Dirty Dirty Dirty
+    """
+
+    def activate(self):
+        """
+        Called when the engine switches to this route.
+        """
+
+        super().activate()
+
+        transport.set_tempo(120)
+        transport.set_cycle('4/4')
+
+        # Setups, banks...
+        seq192.set_screenset(self.name)
+        prodSampler.set_kit(self.name)
+
+        # Microtonality
+        # microtonality.disable()
+        microtonality.enable()
+        microtonality.set_tuning(0.35, 0, 0, 0, 0, 0.35, 0, 0, 0.35, 0, 0, 0)
+
+        # Autotuner Notes
+        notes.set_notes(1, 0, 1, 0, 1, 1, 0, 1, 1, 1, 0, 1)
+
+        # Mk2
+        mk2Control.set_mode('cut_samples', 'cut_synths')
+
+        # Sample
+        self.set_samples_aliases({
+            # 'GuitarCrunch': 'Samples1',
+        })
+
+    def open_samples(self):
+        pass
+        # samples.set('GuitarCrunch', 'Mute', 0.0)
+        # samplesFX3Reverb.set('Trumpets', 'Gain', -10.0)
+
+
+    @pedalboard_button(1)
+    @mk2_button(1, 'blue')
+    def stop(self):
+        """
+        STOP
+        """
+        self.pause_loopers()
+        transport.stop()
+
+
+    @pedalboard_button(2)
+    def intro(self):
+        """
+        INTRO
+        """
+        self.pause_loopers()
+        self.reset()
+
+        # Sequences
+        seq192.select('solo', 'intro_*')
+
+        # Transport
+        transport.start()
+
+        # Samples
+        self.open_samples()
+
+        # Vocals
+        vocalsNano.set('normo_exclu', 'on')
+        vocalsKesch.set('normo_exclu', 'on')
+
+        # Keyboard
+        jmjKeyboard.set_sound('ZTrumpets', boost=True)
+
+
+    @pedalboard_button(3)
+    def refrain(self):
+        """
+        REFRAIN
+        """
+        self.pause_loopers()
+        self.reset()
+
+        # Sequences
+        seq192.select('solo', 'refrain_*')
+
+        # Transport
+        transport.start()
+
+        # Samples
+        self.open_samples()
+
+        # Vocals
+        vocalsNano.set('meuf_exclu', 'on')
+        vocalsKesch.set('normo_exclu', 'on')
+
+        # Keyboard
+        jmjKeyboard.set_sound('ZTrumpets', boost=True)
+
+
+    @pedalboard_button(4)
+    def couplet(self):
+        """
+        COUPLET
+        """
+        self.pause_loopers()
+        self.reset()
+
+        # Sequences
+        seq192.select('solo', 'couplet_*_min')
+
+        # Transport
+        transport.start()
+
+        # Samples
+        self.open_samples()
+
+        # Vocals
+        vocalsNano.set('normo_exclu', 'on')
+        vocalsKesch.set('meuf_exclu', 'on')
+
+        # Keyboard
+        jmjKeyboard.set_sound('ZTrumpets', boost=True)
+
+    @mk2_button(2)
+    def couplet_up(self):
+        """
+        COUPLET UP
+        """
+
+        # Séquences
+        seq192.select('solo', 'couplet_*_up*')
+
+    @mk2_button(3)
+    def couplet_full(self):
+        """
+        COUPLET FULL
+        """
+
+        # Séquences
+        seq192.select('solo', 'couplet_*')
