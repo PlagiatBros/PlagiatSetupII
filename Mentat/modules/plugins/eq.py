44168977362e0eecab286cab41ce468258067d97
jean-emmanuel <jean-emmanuel.doucet@groolot.net>
2025-02-28 20:25:59 +0100

    eq size

--- Mentat/modules/plugins/eq.py
+++ Mentat/modules/plugins/eq.py
@@ -3,10 +3,11 @@ from .default import Plugin
 class AceEQ(Plugin):
 
     def create_parameters(self):
-
+        self.modal['popupHeight']=800
         self.widgets = [{
             'id': self.id + '/eq_fragment',
             'type': 'fragment',
+            'expand': True,
             'file': 'eq.json',
             'props': {
                 'address': '/%s/%s/%s' % (self.mixer_name, self.strip_name, self.name)
