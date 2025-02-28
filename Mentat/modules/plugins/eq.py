fbb7894fc24a49768da1160475213abf9d40bc4e
jean-emmanuel <jean-emmanuel.doucet@groolot.net>
2025-02-28 20:29:05 +0100

    eq address setting

--- Mentat/modules/plugins/eq.py
+++ Mentat/modules/plugins/eq.py
@@ -3,13 +3,17 @@ from .default import Plugin
 class AceEQ(Plugin):
 
     def create_parameters(self):
-        self.modal['popupHeight']=800
+        self.modal['popupHeight'] =800
         self.widgets = [{
             'id': self.id + '/eq_fragment',
             'type': 'fragment',
             'expand': True,
             'file': 'eq.json',
             'props': {
-                'address': '/%s/%s/%s' % (self.mixer_name, self.strip_name, self.name)
+                {
+                    'variables': {
+                        'address': '/%s/%s/%s' % (self.mixer_name, self.strip_name, self.name)
+                    }
+                }
             }
         }]
