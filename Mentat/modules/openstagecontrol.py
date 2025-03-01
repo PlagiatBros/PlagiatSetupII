144e592283f3ae5d880df2e8380c6c8f9b6abdbb
jean-emmanuel <jean-emmanuel.doucet@groolot.net>
2025-03-01 10:52:20 +0100

    osc: make sure non-mie rdefault tab is set on 'Outputs'

--- Mentat/modules/openstagecontrol.py
+++ Mentat/modules/openstagecontrol.py
@@ -214,8 +214,15 @@ class OpenStageControl(Module):
             """
             panel = {'id': 'non-mixer-gui-tabs', 'type': 'panel', 'default': 6, 'tabs': [], 'verticalTabs': True, 'bypass': True, 'onValue': 'var name = getProp(this, "variables").names[value]; if (name) send("/OpenStageControl/call", "set_active_non_mixer", name)'}
             tab_names = []
+            index = 0
             for name, mod in self.engine.modules.items():
                 if isinstance(mod, NonMixer):
+
+                    # non-mixers tabs should always default on 'Outputs'
+                    if name == 'Outputs'
+                        panel['default'] = index
+                    index += 1
+
                     tab = {
                         'type': 'tab',
                         'id': name,
