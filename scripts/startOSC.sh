29be942c0f9b621a2b19252f9bb68eff4db814cd
jean-emmanuel <jean-emmanuel.doucet@groolot.net>
2025-03-01 10:52:54 +0100

    osc: disable osc's own cross client sync mechanism

--- scripts/startOSC.sh
+++ scripts/startOSC.sh
@@ -1,3 +1,3 @@
 cd $PLAGIAT_SETUP_DIR/OpenStageControl
 #taskset -a 0x01 open-stage-control -p 3000 -l hub.json -c custom-module.js -t styles.css -s 127.0.0.1:2001 --disable-gpu --disable-vsync --fullscreen
-open-stage-control -p 3000 -l hub.json -c custom-module.js -t styles.css -s 127.0.0.1:2001 --disable-vsync --fullscreen
+open-stage-control -p 3000 -l hub.json -c custom-module.js -t styles.css -s 127.0.0.1:2001 --disable-vsync --fullscreen --client-options clientSync=0
